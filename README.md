9eb04e5495b4898b681e52991982f688913b6fd4
Abhishek Dubey <abd6982@gmail.com>
2018-11-15 03:39:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+1. Make sure python3 is installed.
+2. Go to the app dir 'firebase_email'.
+3. Create virtual env by executing the command 'python3 -m venv venv'.
+4. Activate virtual env by executing 'source venv/bin/activate'.
+5. Execute 'pip install -r requirements.txt' to install required libraries.
+6. Run the app by executing 'python app.py'.
+
+
+NOTE: Make any changes in the .env file to change variables
---
+++ app.py
@@ -0,0 +1,69 @@
+import imaplib
+import email
+import time
+import os
+import re
+from datetime import datetime
+from os.path import join, dirname
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+EMAIL = os.environ.get('EMAIL')
+PASSWORD = os.environ.get('PASSWORD')
+IMAP_SERVER = os.environ.get('IMAP_SERVER')
+PATH_TO_FIREBASE_CREDENTIALS = os.environ.get('PATH_TO_FIREBASE_CREDENTIALS')
+DATABASE = os.environ.get('DATABASE')
+POLLING_INTERVAL = int(os.environ.get('POLLING_INTERVAL'))
+
+
+cred = credentials.Certificate(PATH_TO_FIREBASE_CREDENTIALS)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def store_in_firestore(from_mail, mail):
+  print('Storing new email')
+  print(from_mail)
+  print(mail)
+  db.collection(DATABASE).document(from_mail).set(mail)
+
+def check_new_mails():
+  mail = imaplib.IMAP4_SSL(IMAP_SERVER)
+  (retcode, capabilities) = mail.login(EMAIL, PASSWORD)
+  mail.list()
+  mail.select('inbox')
+  (retcode, mails) = mail.search(None, '(UNSEEN)')
+  if retcode == 'OK':
+    for num in mails[0].split() :
+      typ, data = mail.fetch(num,'(RFC822)')
+      for response_part in data:
+        if isinstance(response_part, tuple):
+          new_mail = email.message_from_string(response_part[1].decode('utf-8'))
+          body = ''
+          try:
+            body = new_mail.get_payload(decode=True).decode('utf-8')
+          except:
+            body = new_mail.get_payload(0)
+            while body.is_multipart():
+              body = body.get_payload(0)
+            body = body.get_payload(decode=True).decode('utf-8')
+          store_in_firestore(
+            datetime.strptime(new_mail['date'], '%a, %d %b %Y %H:%M:%S %z').strftime('%d %b %Y, %H:%M:%S %z'),
+            {
+              'customerEmail': re.sub(r'[<>]', '', new_mail['from']).split()[-1],
+              'subject': new_mail['subject'],
+              'body': body
+            }
+          )
+          typ, data = mail.store(num,'+FLAGS','\\Seen')
+
+if __name__ == '__main__':
+  while True:
+    check_new_mails()
+    time.sleep(POLLING_INTERVAL)
+
